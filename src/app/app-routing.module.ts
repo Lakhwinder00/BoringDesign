17d0446e4ff5abe09a8f5144acfc21db923b8498
lakhwinder <lakhwinder@podtech,com>
2023-03-12 18:57:21 +0530

    Add menu om menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,7 +23,7 @@ const routes: Routes = [
 ]
   },
   { path: 'process', component: ProcessFileComponent, canActivate: [AuthGuard] },
-  { path: 'myaccount', component: MyAccountComponent, canActivate: [AuthGuard] }
+  { path: 'my-account', component: MyAccountComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
